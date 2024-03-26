dd06e69ef8ab9193cb13b56feae2d0729a124881
dylanswin <105040962@student.swin.edu.au>
2024-03-26 20:39:58 +1100

    Committing thus far the route 53 stack code

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ infra_repo_id="da2667/rig-infra"
 frontend_repo_id="da2667/rig"
 api_repo_id="da2667/rig-api"
 ami_id="ami-02eec49345a878486"
+hosted_zone_id="Z06281253VPT3IJVNJVUU"
 
 #echo "Deploying infra pipeline..."
 #aws cloudformation deploy \
@@ -29,6 +30,8 @@ vpc_id=$(aws cloudformation --region $region describe-stacks --stack-name rig-${
 
 frontend_subnet_id=$(aws ec2 describe-subnets --filter Name=vpc-id,Values=$vpc_id Name=cidr-block,Values="10.0.0.0/24" --query 'Subnets[*].[SubnetId]' --output text)
 api_subnet_id=$(aws ec2 describe-subnets --filter Name=vpc-id,Values=$vpc_id Name=cidr-block,Values="10.0.1.0/24" --query 'Subnets[*].[SubnetId]' --output text)
+db_subnet_1_id=$(aws ec2 describe-subnets --filter Name=vpc-id,Values=$vpc_id Name=cidr-block,Values="10.0.2.0/24" --query 'Subnets[*].[SubnetId]' --output text)
+db_subnet_2_id=$(aws ec2 describe-subnets --filter Name=vpc-id,Values=$vpc_id Name=cidr-block,Values="10.0.3.0/24" --query 'Subnets[*].[SubnetId]' --output text)
 
 echo "Deploying security groups..."
 aws cloudformation deploy \
@@ -53,6 +56,8 @@ aws cloudformation deploy \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides SGName="rig-${env}-db-sg" SGDescription="Database security group access from API for the Rig application" VpcId=$vpc_id InboundSG=$api_sg_id InboundPort=3306
 
+db_sg_id=$(aws ec2 describe-security-groups --filter Name=vpc-id,Values=$vpc_id Name=group-name,Values=rig-${env}-db-sg --query 'SecurityGroups[*].[GroupId]' --output text)
+
 echo "Deploying EC2 instances..."
 
 aws cloudformation deploy \
@@ -62,6 +67,7 @@ aws cloudformation deploy \
     --parameter-overrides SecurityGroup=$frontend_sg_id KeyPairName="rig-${env}-frontend-keypair" ImageId=$ami_id InstanceType="t2.medium" InstanceName="rig-${env}-frontend-instance" SubnetId=$frontend_subnet_id
 
 frontend_instance_id=$(aws ec2 describe-instances --filter Name=subnet-id,Values=$frontend_subnet_id --query 'Reservations[*].Instances[*].[InstanceId]' --output text)
+frontend_instance_ip=$(aws ec2 describe-addresses --filter Name=instance-id,Values=$frontend_instance_id --query 'Addresses[0].PublicIp' --output text)
 
 aws cloudformation deploy \
     --stack-name rig-api-instance-stack \
@@ -69,8 +75,17 @@ aws cloudformation deploy \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides SecurityGroup=$api_sg_id KeyPairName="rig-${env}-api-keypair" ImageId=$ami_id InstanceType="t2.medium" InstanceName="rig-${env}-api-instance" SubnetId=$api_subnet_id
 
-# echo "Deploying RDS..."
-# aws cloudformation deploy --stack-name rig-DB-RDS-Stack --template-file ./infra/rds/rig-DB-RDS.yml --capabilities CAPABILITY_NAMED_IAM
+api_instance_id=$(aws ec2 describe-instances --filter Name=subnet-id,Values=$api_subnet_id --query 'Reservations[*].Instances[*].[InstanceId]' --output text)
+api_instance_ip=$(aws ec2 describe-addresses --filter Name=instance-id,Values=$api_instance_id --query 'Addresses[0].PublicIp' --output text)
+
+echo "Deploying RDS..."
+aws cloudformation deploy \
+    --stack-name rig-DB-RDS-Stack \
+    --template-file ./infra/rds/rds.yml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides Environment=${env} AllocatedStorage=20 AZ="ap-southeast-2a" DBSubnet1=$db_subnet_1_id DBSubnet2=$db_subnet_2_id DBInstanceClass="db.t2.micro" DBName=rigdb DBPort=3306 DBUser=dbadmin DBSecurityGroup=$db_sg_id
+
+db_instance_hostname=$(aws rds describe-db-instances --db-instance-identifier rig-${env}-db --query 'DBInstances[0].Endpoint.Address' --output text)
 
 echo "Deploying CloudWatch Monitoring stack..."
 aws cloudformation deploy \
@@ -92,3 +107,9 @@ aws cloudformation deploy \
     --template-file ./infra/pipeline/api_pipeline.yml \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides CodePipelineName="rig-${env}-api-codepipeline" ApiRepo=$api_repo CodeBuildImage="aws/codebuild/amazonlinux2-x86_64-standard:5.0" BucketName="rig-${env}-api-artifacts-bucket-210023018938" CodeStarConnectionArn=$codestar_arn ApiGitHubRepoId=$api_repo_id GitHubBranch=$env ApplicationName="rig-${env}-api-application" InstanceName="rig-${env}-api-instance"
+
+aws cloudformation deploy \
+    --stack-name rig-${env}-route53-stack \
+    --template-file ./infra/r53/route53.yml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides Environment=${env} DomainName="rigapp.io" HostedZoneId=${hosted_zone_id} FrontendInstanceIP=$frontend_instance_ip APIInstanceIP=$api_instance_ip DBInstanceHostname=$db_instance_hostname
